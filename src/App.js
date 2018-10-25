79f1648cf6da24e27ca8d55a53313da1295658bb
Joseph Bartone <jrbartone@wpi.edu>
2018-10-25 11:50:08 +0200

    created map_container.js, added placeholder fb init code

--- src/App.js
+++ src/App.js
@@ -3,6 +3,22 @@ import BottomBar from "./containers/bottom_bar";
 import SideMenu from "./containers/side_menu";
 
 class App extends Component {
+  // Links the firebase to react app
+  // Fill in the fields so it works
+  constructor(props){
+    super(props);
+    var config = {
+      apiKey: "",
+      authDomain: "",
+      databaseURL: "",
+      projectId: "appdividend-c8a0f",
+      storageBucket: "",
+      messagingSenderId: ""
+    };
+    firebase.initializeApp(config);
+  }
+
+
   render() {
     return (
       <div>
@@ -11,6 +27,7 @@ class App extends Component {
       </div>
     );
   }
+ 
 }
 
 export default App;
